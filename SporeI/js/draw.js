e127b39e045038843825c1bc466383713046f125
RSL <remy.saintlouboue@gmail.com>
2018-09-27 09:56:33 +0200

    Base Code

---
+++ SporeI/js/draw.js
@@ -0,0 +1,39 @@
+﻿function drawStats() {
+	$('#cellPower').text(cellPower);
+
+	if (cellLvl === 50) {
+		$('#cellLvl').text('MAX');
+	}
+	else {
+		$('#cellLvl').text(cellLvl);
+	}
+	$('#food').text(getRegime());
+	$('#xpToLvl').text(xpToLvlUp);
+	$('#cell').css('color', color);
+	if (buyTimeBuyed) {
+		$('#secBeforeMutation').text(secBeforeMutation);
+	}
+}
+
+function drawPowerUps() {
+	if (buySaveBuyed && buyFoodBuyed) {
+		$('#allowEvol').show();
+	}
+}
+
+function drawError() {
+	if (errorTimer > 0) {
+		$('#errorDiv').show();
+		errorTimer--;
+	}
+	else {
+		$('#errorDiv').hide();
+		$('#errorLabel').text('');
+	}
+}
+
+function drawAll() {
+	drawStats();
+	drawPowerUps();
+	drawError();
+}
---
+++ SporeI/js/variables.js
@@ -0,0 +1,14 @@
+﻿//Global variables of SporeIdle
+var mobEaten;
+var plantEaten;
+var color;
+var sporeTimer;
+var mutationTimer;
+var xpToLvlUp;
+var secBeforeMutation;
+var errorTimer;
+
+var buyTimeBuyed;
+var buyFoodBuyed;
+var buySaveBuyed;
+var allowEvolBuyed;
